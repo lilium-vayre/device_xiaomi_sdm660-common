3d940c53706a80ae06e8f28a7d810f94bd730ac1
Albert I <kras@raphielgang.org>
2022-06-19 12:29:52 +0400

    sdm660-common: Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- sdm660.mk
+++ sdm660.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+
 # Enforce vintf kernel requirements
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
